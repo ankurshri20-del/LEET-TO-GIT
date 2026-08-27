07de9b8bbdb2c412f7654dc5bbd9a86e0ebfe4f0
ankurshri20-del <ankur.shri20@gmail.com>
2026-08-27 18:38:59 +0530

    Time: 37 ms (96.54%) | Memory: 29 MB (92.41%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        int n = nums.size();
+        
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < n - 2; i++) {
+            // Since sorted, if nums[i] > 0, no triplet can sum to 0
+            if (nums[i] > 0) break;
+            
+            // Skip duplicate values for i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int left = i + 1, right = n - 1;
+            
+            while (left < right) {
+                long sum = (long)nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    
+                    // Skip duplicates for left and right
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        
+        return result;
+    }
+};
